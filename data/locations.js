853765e119040884e27d47d2611485af3c4723b7
Daniel Z <danielz000@hotmail.com>
2019-05-18 01:12:38 +0100

    Uploading all BDIS files from PHARMWEB

---
+++ data/locations.js
@@ -0,0 +1,21 @@
+[
+{"locationcode": 1, "name": "EMERGENCY TRAY AREA"},
+{"locationcode": 3, "name": "SPH PHARMACY"},
+{"locationcode": 4, "name": "DO NOT USE (COMM.)"},
+{"locationcode": 6, "name": "CYTO'S 2ND FLOOR CC"},
+{"locationcode": 9, "name": "TPN/CIVA 3RD FL C.C"},
+{"locationcode": 11, "name": "RNTNE PHARMACY DISP."},
+{"locationcode": 14, "name": "UCL PHARM.IN-PATIENT"},
+{"locationcode": 17, "name": "HOLDING AREA FOR RTN"},
+{"locationcode": 18, "name": "REPACKING UNIT"},
+{"locationcode": 19, "name": "UCL PHARM.OUTPATIENT"},
+{"locationcode": 20, "name": "CLINICAL TRIAL / IMP"},
+{"locationcode": 22, "name": "QUEENS SQ PHARMACY"},
+{"locationcode": 23, "name": "HIGHGATE DISPENSARY"},
+{"locationcode": 24, "name": "LLOYDS @CANCER CENTRE"},
+{"locationcode": 25, "name": "UCL TABLETING UNIT"},
+{"locationcode": 26, "name": "CLINICAL TRIALS NHNN"},
+{"locationcode": 28, "name": "MORTIMER MKT PHARM."},
+{"locationcode": 30, "name": "UCH @WESTMORELAND ST"},
+{"locationcode": 43, "name": "WARD STOCK TTO ONLY"}
+]
